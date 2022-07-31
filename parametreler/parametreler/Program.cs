92924ebea7fb1660099e107051eb17131224b2ca
Yasin <107244487+masora2@users.noreply.github.com>
2022-07-31 16:16:25 +0300

    parametreler

---
+++ parametreler/parametreler/Program.cs
@@ -0,0 +1,24 @@
+﻿string sayi = "999";
+
+bool sonuc = int.TryParse(sayi, out int outSayi);
+
+if (sonuc)
+{
+    Console.WriteLine("Başarılı! ");
+    Console.WriteLine(outSayi + 20);
+}
+
+else
+{
+    Console.WriteLine("Başarısız!");
+}
+Metotlar instance = new Metotlar();
+instance.Topla(4, 5, out int toplamSonucu);
+Console.WriteLine(toplamSonucu);
+class Metotlar
+{
+    public void Topla(int a,int b,out int toplam)
+    {
+        toplam = a + b;
+    }
+}
